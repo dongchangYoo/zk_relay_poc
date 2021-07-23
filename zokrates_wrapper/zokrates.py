462b4d1bae2103d465764bf26c87cf92b7dab19f
dongchangYoo <dongchang.yoo@pi-lab.io>
2021-07-23 17:24:04 +0900

    apply config file as toml format

--- zokrates_wrapper/zokrates.py
+++ zokrates_wrapper/zokrates.py
@@ -1,6 +1,8 @@
 import json
 import os
 import subprocess
+import toml
+
 
 """
 <Data architecture>
@@ -25,24 +27,40 @@ data
 
 
 class Zokrates:
-    def __init__(self, zokrates_bin_path: str = None, zokrates_stdlib_path: str = None, out_dir: str = None):
-        with open("../conf/zk_relay_config.json", "r") as json_data:
-            config = json.load(json_data)
-            if zokrates_bin_path is None:
-                self.zokrates_bin_path = config["zokrates_bin_path"]
-            if zokrates_stdlib_path is None:
-                self.zokrates_stdlib_path = config["zokrates_stdlib_path"]
-            if out_dir is None:
-                if not os.path.exists("../out"):
-                    os.mkdir("../out")
-                self.out_dir = "../out/"
-        self.basic_cmd = self.zokrates_bin_path + "/zokrates"
+    def __init__(self, config_path: str):
+        self.config = toml.load(config_path)
+        context = self.config["context"]
 
-    def compile(self, file_path: str):
-        cmd = [self.basic_cmd, "compile"]
-        cmd += ["-i", file_path]
-        cmd += ["-o", self.out_dir + "program"]
-        cmd += ["--stdlib-path", self.zokrates_stdlib_path]
+        program_root_dir = context["ROOT_DIR"]
+        program_ctx = context["program"]
+        self.code_dir = program_root_dir + program_ctx["CODE_DIR"]
+        self.code_path = self.code_dir + program_ctx["CODE_FILE_NAME"]
+        self.prog_path = self.code_dir + program_ctx["PROGRAM_FILE_NAME"]
+
+        setup_ctx = context["setup"]
+        self.setup_dir = program_root_dir + setup_ctx["SETUP_DIR"]
+        self.vkey_path = self.setup_dir + setup_ctx["VKEY_FILE_NAME"]
+        self.pkey_path = self.setup_dir + setup_ctx["PKEY_FILE_NAME"]
+
+        proof_ctx = context["proof"]
+        self.proof_dir = program_root_dir + proof_ctx["PROOF_DIR"]
+        self.witness_path = self.proof_dir + proof_ctx["WITNESS_FILE_NAME"]
+        self.proof_path = self.proof_dir + proof_ctx["PROOF_FILE_NAME"]
+
+        verify_ctx = context["verify"]
+        self.verifier_dir = program_root_dir + verify_ctx["VERIFICATION_DIR"]
+        self.verifier_contract_path = self.verifier_dir + verify_ctx["CONTRACT_FILE_NAME"]
+
+        self.zokrates_bin_path = self.config["zokrates"]["BIN_PATH"]
+        self.zokrates_std_lib_path = self.config["zokrates"]["STDLIB_PATH"]
+
+    def compile(self):
+        cmd = [self.zokrates_bin_path, "compile"]
+        cmd += ["-i", self.code_path]
+        if not os.path.exists(self.proof_dir):
+            os.mkdir(self.proof_dir)
+        cmd += ["-o", self.proof_path]
+        cmd += ["--stdlib-path", self.zokrates_std_lib_path]
 
         print(">> Compile the program to r1cs form.")
         result = subprocess.run(cmd)
@@ -52,5 +70,5 @@ class Zokrates:
 
 
 if __name__ == "__main__":
-    zk = Zokrates()
-    zk.compile("../test/root.zok")
+    zk = Zokrates("../conf/config.toml")
+    zk.compile()
